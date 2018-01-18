8122c67224ee6108bf3495944481511a0d6e2f7e
Eik Emil Christensen <me@eikc.dk>
2018-01-18 23:23:46 +0100

    added chmod for common files before running them

--- 00-start.sh
+++ 00-start.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+echo "Giving correct permissions..."
+chmod +x ./01-brew.sh;
+chmod +x ./02-npm.sh;
+chmod +x ./03-docker.sh;
+
+
+echo "Starting installation..."
 ./01-brew.sh;
 ./02-npm.sh;
 
