a0c5fa4aeeb2598794b6be9ce4f329af7a053083
Eik Emil Christensen <me@eikc.dk>
2018-01-18 23:07:37 +0100

    added basic brew setup for my simple dev setup
    .

---
+++ 01-brew.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+# Install brew if is not already Installed
+if test ! $(which brew); then
+  	echo "Installing homebrew..."
+  	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+echo ""
+echo "Updating homebrew"
+brew update
+
+echo "Installing cask "
+brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+
+echo 'export HOMEBREW_CASK_OPTS="--appdir=/Applications"' >> ~/.zshrc
+
+
+# Some cask apps
+apps=(
+    chrome
+	firefoxdeveloperedition
+	slack
+	spotify
+	messenger-native
+)
+
+echo "Installing apps..."
+brew cask install ${apps[@]}
+
+# Development
+devel_apps=(
+	docker
+    gitkraken
+	visual-studio-code
+)
+echo "Installing development apps..."
+brew cask install ${devel_apps[@]}
+
+echo "installing vs code extensions..."
+vs_code_extensions=(
+    auiworks.amvim
+    felipe.nasc-touchbar
+    robertohuertasm.vscode-icons
+    Angular.ng-template
+    PeterJausovec.vscode-docker
+    christian-kohler.npm-intellisense
+    eg2.tslint
+    eg2.vscode-npm-script
+    michelemelluso.code-beautifier
+    lukehoban.Go
+    WallabyJs.quokka-vscode
+    WallabyJs.wallaby-vscode
+)
+
+for i in "${vs_code_extensions[@]}"
+do
+   code --install-extension $i
+done
+
+echo "installing development languages and tooling..."
+languages=(
+	nodejs
+	go
+	dep
+)
+
+brew install ${languages[@]}
+
+echo "You are ready to code master..."
