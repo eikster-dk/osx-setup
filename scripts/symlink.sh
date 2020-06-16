3041755673989c6aac2aaa08a8ecb44087eec3da
Eik Emil Christensen <me@eikc.dk>
2020-06-16 08:46:54 +0200

    fixed minor symlink bug

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -3,5 +3,5 @@ ln -sfn $(pwd)/vs-code/settings.json ~/Library/Application\ Support/Code/User/se
 ln -sfn $(pwd)/vs-code/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 ln -sfn $(pwd)/vs-code/snippets/ ~/Library/Application\ Support/Code/User
 ln -sfn $(pwd)/zsh/.zshrc ~/.zshrc
-ln -sfn $(pwd)/zsh/aliases ~/.aliases
+ln -sfn $(pwd)/zsh/aliases.zsh ~/.aliases
 ln -sfn $(pwd)/zsh/.p10k.zsh ~/.p10k.zsh
