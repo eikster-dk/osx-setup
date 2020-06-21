e03ba2a953c907638f3e00b172d57b50a9cb2625
Eikc <me@eikc.dk>
2020-06-21 15:10:50 +0200

    minor tweak to zsh/powerlevel installation

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -1,5 +1,8 @@
 echo "installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+echo "setting up powerlevel 10k theme"
+echo 'source /usr/local/opt/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
+
 echo "setting up zsh"
 chsh -s /usr/local/bin/zsh
